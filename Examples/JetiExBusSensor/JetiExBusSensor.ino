de0648a70a41d79bfbcf72fd5e257ab36b7cd37d
Sepp62 <33748087+Sepp62@users.noreply.github.com>
2018-02-09 21:10:03 +0100

    Support for AtMega32u4 added (2)

--- Examples/JetiExBusSensor/JetiExBusSensor.ino
+++ Examples/JetiExBusSensor/JetiExBusSensor.ino
@@ -7,7 +7,8 @@
   
   Version history:
   0.90   02/04/2018  created
-  
+  0.91   02/09/2018  Support for AtMega32u4 added
+
   Permission is hereby granted, free of charge, to any person obtaining
   a copy of this software and associated documentation files (the "Software"),
   to deal in the Software without restriction, including without limitation
